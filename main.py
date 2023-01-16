b6bf73d3170125c017e95ae15cef7529b79a1344
Nizar <therealnizarbenalla@gmail.com>
2023-01-16 23:32:58 +0100

    return distances from dict

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+import functools
+import math
+
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWebEngineWidgets import QWebEngineView # pip install PyQtWebEngine
+from PyQt5.QtWebEngineWidgets import QWebEngineView  # pip install PyQtWebEngine
 import sys
 import io
 import numpy as np
 import random
 import pandas as pd
-from geopy import distance,geocoders,Nominatim # to calculate distance on the surface
+from geopy import distance, geocoders, Nominatim  # to calculate distance on the surface
 import folium
 from folium.plugins import MarkerCluster
 from datetime import datetime
 import time
 import h3
+
+
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Calcule de plus court chemin")
@@ -52,10 +57,10 @@ class Ui_Dialog(object):
         self.pushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.pushButton.setAutoFillBackground(False)
         self.pushButton.setStyleSheet("background-color:rgb(79, 200, 255);\n"
-"color:#fff;\n"
-"font-weight:bold;\n"
-"border:5 px solid #eee;\n"
-"border-radius: 8px")
+                                      "color:#fff;\n"
+                                      "font-weight:bold;\n"
+                                      "border:5 px solid #eee;\n"
+                                      "border-radius: 8px")
         self.pushButton.setCheckable(True)
         self.pushButton.setDefault(True)
         self.pushButton.setObjectName("pushButton")
@@ -71,10 +76,10 @@ class Ui_Dialog(object):
         self.pushButton_2.setGeometry(QtCore.QRect(230, 510, 181, 41))
         self.pushButton_2.setAutoFillBackground(False)
         self.pushButton_2.setStyleSheet("background-color:rgb(79, 200, 255);\n"
-"color:#fff;\n"
-"font-weight:bold;\n"
-"border-radius: 5px;\n"
-"border:5 px solid #eee")
+                                        "color:#fff;\n"
+                                        "font-weight:bold;\n"
+                                        "border-radius: 5px;\n"
+                                        "border:5 px solid #eee")
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_2.clicked.connect(self.upload)
 
@@ -87,7 +92,7 @@ class Ui_Dialog(object):
         font.setBold(True)
         self.label_6.setFont(font)
         self.label_6.setStyleSheet("color:#888")
-        self.label_6.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.label_6.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
         self.label_6.setObjectName("label_6")
         self.label_7 = QtWidgets.QLabel(self.frame)
         self.label_7.setGeometry(QtCore.QRect(50, 170, 391, 201))
@@ -113,7 +118,7 @@ class Ui_Dialog(object):
         self.label_9.setStyleSheet("color:#888")
         self.label_9.setObjectName("label_9")
         self.label_10 = QtWidgets.QLabel(self.frame)
-        self.label_10.setGeometry(QtCore.QRect(180,380, 151, 31))
+        self.label_10.setGeometry(QtCore.QRect(180, 380, 151, 31))
         self.label_10.setStyleSheet("color:#888;")
         self.label_10.setObjectName("label_10")
         self.label_10.setFont(font)
@@ -131,8 +136,9 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Calcule de plus court chemin"))
-        self.label_2.setText(_translate("Dialog", "L\'objectif du projet est de développer une application avec python permettant d’utiliser les algorithmes\n"
-"génétiques pour trouver la distance minimale du chemin effectué par le bus du ramassage scolaire."))
+        self.label_2.setText(_translate("Dialog",
+                                        "L\'objectif du projet est de développer une application avec python permettant d’utiliser les algorithmes\n"
+                                        "génétiques pour trouver la distance minimale du chemin effectué par le bus du ramassage scolaire."))
         self.label.setText(_translate("Dialog", "Final Projet"))
         self.pushButton.setText(_translate("Dialog", "Calculer le PCC"))
         self.label_3.setText(_translate("Dialog", "Upload le Dataset :"))
@@ -154,16 +160,15 @@ class Ui_Dialog(object):
         self.webView.setGeometry(QtCore.QRect(520, 160, 501, 361))
 
     def upload(self):
-        fname = QtWidgets.QFileDialog.getOpenFileName(self.frame,'select dataset','.','csv files(*.csv)')
+        fname = QtWidgets.QFileDialog.getOpenFileName(self.frame, 'select dataset', '.', 'csv files(*.csv)')
         if fname:
             self.fname = str(fname[0])
             self.pushButton.setEnabled(True)
             self.pushButton.clicked.connect(self.main)
             print(self.fname)
-    
-    
+
     def init_geo(self):
-            # Initialiser le géolocalisateur en utilisant l'email de l'utilisateur comme agent
+        # Initialiser le géolocalisateur en utilisant l'email de l'utilisateur comme agent
         geolocator = geocoders.Nominatim(user_agent="mail@myserver.com")
         # Initialiser une liste pour stocker les coordonnées
         self.label_6.setText("Le Plus cours Chemin :")
@@ -173,8 +178,8 @@ class Ui_Dialog(object):
         self.label_9.update()
         self.label_10.update()
         QtCore.QCoreApplication.processEvents()
-        coordinates_list=[]
-        self.n_cities = 10
+        coordinates_list = []
+        self.n_cities = 18
         self.n_population = 50
         if isinstance(self.lineEdit.text(), int) or isinstance(self.lineEdit.text(), float):
             self.mutation_rate = int(self.lineEdit.text())
@@ -184,7 +189,7 @@ class Ui_Dialog(object):
             self.lineEdit.update()
             QtCore.QCoreApplication.processEvents()
         df = pd.read_csv(self.fname)
-        df = df.sample(frac=1 , random_state=1).reset_index()
+        df = df.sample(frac=1, random_state=1).reset_index()
         # Récupérer la liste des noms de villes à partir d'un dataframe
         self.names_list = np.array(df['Street'].head(self.n_cities))
         # Boucler sur toutes les villes pour récupérer les coordonnées
@@ -192,20 +197,28 @@ class Ui_Dialog(object):
             location = geolocator.geocode(city)
             coordinates_list.append(list((location.latitude, location.longitude)))
         # Créer un dictionnaire pour stocker les noms de villes et les coordonnées
-        self.cities_dict = { x:y for x,y in zip(self.names_list,coordinates_list)}
-        
+        self.cities_dict = {x: y for x, y in zip(self.names_list, coordinates_list)}
+        print(self.cities_dict)
+        self.distances ={}
+        self.create_distance_dict()
 
     # Fonction pour calculer la distance entre deux points
-    def compute_city_distance_coordinates(self,a,b):
-        return h3.point_dist(a, b) # to get distance in meters
+    def create_distance_dict(self):
+        # nested loop to iterate through all coordinates
+        for point1, coord1 in self.cities_dict.items():
+            for point2, coord2 in self.cities_dict.items():
+                if point1 != point2:
+                    distance = math.dist(coord1, coord2)
+                    self.distances[(point1, point2)] = distance
+    def compute_city_distance_names(self, city_a, city_b):
+        if (city_a, city_b) in self.distances.keys():
+            return self.distances[(city_a, city_b)]
+        elif (city_b, city_a) in self.distances.keys():
+            return self.distances[(city_b, city_a)]
+        else:
+            raise ValueError(f"Distance between {city_a} and {city_b} not found in dictionary.")
 
-
-    # Fonction pour calculer la distance entre deux villes en utilisant les coordonnées stockées dans le dictionnaire
-    def compute_city_distance_names(self,city_a, city_b, cities_dict):
-        return self.compute_city_distance_coordinates(cities_dict[city_a], cities_dict[city_b])
-    
-    
-    def genesis(self,city_list, n_population):
+    def genesis(self, city_list, n_population):
         # Initialiser une liste vide pour stocker les solutions générées
         population_set = []
         # Itérer à travers le nombre de population spécifié
@@ -214,68 +227,66 @@ class Ui_Dialog(object):
             sol_i = city_list[np.random.choice(list(range(self.n_cities)), self.n_cities, replace=False)]
             population_set.append(sol_i)
         return np.array(population_set)
-    
-    
-    def fitness_eval(self,city_list, cities_dict):
+
+    def fitness_eval(self,city_list):
         # Initialiser une variable pour stocker la distance totale
         total = 0
         # Itérer à travers les villes dans la liste sauf la dernière
-        for i in range(self.n_cities-1):
+        for i in range(self.n_cities - 1):
             # Récupérer la ville actuelle et la prochaine
             a = city_list[i]
-            b = city_list[i+1]
+            b = city_list[i + 1]
             # Ajouter la distance entre les deux villes au total
-            total += self.compute_city_distance_names(a,b, cities_dict)
+            total += self.compute_city_distance_names(a, b)
         # Retourner la distance totale
         return total
-    
-    
-    def get_all_fitnes(self,population_set, cities_dict):
+
+    def get_all_fitnes(self, population_set, cities_dict):
         # Initialiser une liste pour stocker les valeurs de fitness
         fitnes_list = np.zeros(self.n_population)
 
         # Boucler sur toutes les solutions en calculant le fitness pour chaque solution
-        for i in  range(self.n_population):
-            fitnes_list[i] = self.fitness_eval(population_set[i], self.cities_dict)
+        for i in range(self.n_population):
+            fitnes_list[i] = self.fitness_eval(population_set[i])
 
         # retourner la liste des fitness
         return fitnes_list
-    
-    
-    def progenitor_selection(self,population_set,fitnes_list):
+
+
+
+    def progenitor_selection(self, population_set, fitnes_list):
         # calculer la somme de tous les fitness
         total_fit = fitnes_list.sum()
         # calculer la probabilité pour chaque solution
-        prob_list = fitnes_list/total_fit
+        prob_list = fitnes_list / total_fit
 
         # Il y a une chance qu'un progéniteur s'accouple avec soi-même
-        progenitor_list_a = np.random.choice(list(range(len(population_set))), len(population_set),p=prob_list, replace=True)
-        progenitor_list_b = np.random.choice(list(range(len(population_set))), len(population_set),p=prob_list, replace=True)
+        progenitor_list_a = np.random.choice(list(range(len(population_set))), len(population_set), p=prob_list,
+                                             replace=True)
+        progenitor_list_b = np.random.choice(list(range(len(population_set))), len(population_set), p=prob_list,
+                                             replace=True)
 
         # selectionner les solutions progénitrices
         progenitor_list_a = population_set[progenitor_list_a]
         progenitor_list_b = population_set[progenitor_list_b]
 
         # retourner la liste des progénitrices
-        return np.array([progenitor_list_a,progenitor_list_b])
-    
-    
-    def mate_progenitors(self,prog_a, prog_b):
+        return np.array([progenitor_list_a, progenitor_list_b])
+
+    def mate_progenitors(self, prog_a, prog_b):
         # prendre les 5 premières villes de la solution progénitrice A
         offspring = prog_a[0:5]
-
         # Pour chaque ville de la solution progénitrice B
         for city in prog_b:
             # Si la ville n'est pas dans la descendance
             if not city in offspring:
                 # Ajouter la ville à la descendance
-                offspring = np.concatenate((offspring,[city]))
+                offspring = np.concatenate((offspring, [city]))
 
         # retourner la descendance
         return offspring
 
-    
-    def mate_population(self,progenitor_list):
+    def mate_population(self, progenitor_list):
         # Initialiser une liste pour stocker la nouvelle population
         new_population_set = []
         # Boucler sur toutes les solutions progénitrices
@@ -289,13 +300,12 @@ class Ui_Dialog(object):
         # retourner la nouvelle population
         return new_population_set
 
-
-    def mutate_offspring(self,offspring):
+    def mutate_offspring(self, offspring):
         # Boucler sur un certain nombre de positions, déterminé par le taux de mutation
-        for q in range(int(self.n_cities*self.mutation_rate)):
+        for q in range(int(self.n_cities * self.mutation_rate)):
             # choisir aléatoirement deux positions
-            a = np.random.randint(0,self.n_cities)
-            b = np.random.randint(0,self.n_cities)
+            a = np.random.randint(0, self.n_cities)
+            b = np.random.randint(0, self.n_cities)
 
             # Echanger les villes à ces positions
             offspring[a], offspring[b] = offspring[b], offspring[a]
@@ -303,8 +313,7 @@ class Ui_Dialog(object):
         # retourner l'enfant muté
         return offspring
 
-    
-    def mutate_population(self,new_population_set):
+    def mutate_population(self, new_population_set):
         # Initialiser une liste pour stocker la population mutée
         mutated_pop = []
         # Boucler sur tous les enfants
@@ -313,60 +322,60 @@ class Ui_Dialog(object):
             mutated_pop.append(self.mutate_offspring(offspring))
         # retourner la population mutée
         return mutated_pop
-    
-    
-    def plot_map(self): 
-        if self.iteration!=0:
-            self.label_5.setText(f"iteration numero {self.iteration*100}")
-        
-        self.label_10.setText(f"{round(self.best_solution[1],3)} Km")
+
+    def plot_map(self):
+        if self.iteration != 0:
+            self.label_5.setText(f"iteration numero {self.iteration * 100}")
+
+        self.label_10.setText(f"{round(self.best_solution[1], 3)} Km")
         self.label_7.setText(str(self.best_road))
         self.label_10.update()
         self.label_7.update()
         self.label_5.update()
         QtCore.QCoreApplication.processEvents()
         time.sleep(1)
-        
-        
-        
+
     def main(self):
         self.init_geo()
         population_set = self.genesis(self.names_list, self.n_population)
-        fitnes_list = self.get_all_fitnes(population_set,self.cities_dict)
-        progenitor_list = self.progenitor_selection(population_set,fitnes_list)
+        fitnes_list = self.get_all_fitnes(population_set, self.cities_dict)
+        progenitor_list = self.progenitor_selection(population_set, fitnes_list)
         new_population_set = self.mate_population(progenitor_list)
         mutated_pop = self.mutate_population(new_population_set)
-        self.best_solution = [-1,np.inf,np.array([])]
+        self.best_solution = [-1, np.inf, np.array([])]
         self.iteration = 0
         for i in range(1000):
-            #Saving the best solution
+            # Saving the best solution
             if fitnes_list.min() < self.best_solution[1]:
                 self.best_solution[0] = i
                 self.best_solution[1] = fitnes_list.min()
                 self.best_solution[2] = np.array(mutated_pop)[fitnes_list.min() == fitnes_list]
-            progenitor_list = self.progenitor_selection(population_set,fitnes_list)
+            progenitor_list = self.progenitor_selection(population_set, fitnes_list)
             new_population_set = self.mate_population(progenitor_list)
             mutated_pop = self.mutate_population(new_population_set)
             self.best_road = self.best_solution[2][0]
-            if i%100==0: 
+            if i % 100 == 0:
                 self.iteration += 1
                 print(i, fitnes_list.min(), fitnes_list.mean(), datetime.now().strftime("%d/%m/%y %H:%M"))
                 self.plot_map()
-            fitnes_list = self.get_all_fitnes(mutated_pop,self.cities_dict)
-            
+            fitnes_list = self.get_all_fitnes(mutated_pop, self.cities_dict)
+
         self.m.location = self.cities_dict[self.best_road[0]]
         loc = []
         for el in self.best_road:
             loc.append(self.cities_dict[el])
             folium.Marker(
-              self.cities_dict[el], popup=f"<i>{el}</i>").add_to(self.m)
+                self.cities_dict[el], popup=f"<i>{el}</i>").add_to(self.m)
         loc.append(self.cities_dict[self.best_road[0]])
         folium.PolyLine(loc,
-                color='blue',
-                weight=4,
-                opacity=0.9).add_to(self.m)
+                        color='blue',
+                        weight=4,
+                        opacity=0.9).add_to(self.m)
         self.webView.setHtml(self.m.get_root().render())
-            
+
+
+
+
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
